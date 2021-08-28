de0f8a048c0c18bf802b64e6208bd11e2e1ce324
Midhun EM <midhunem42@gmail.com>
2021-08-28 19:30:44 +0530

    [delete] unused removed items

--- src/store/globalSaga.ts
+++ src/store/globalSaga.ts
@@ -1,12 +1,1 @@
-import { call, delay, put, select, takeLatest } from "redux-saga/effects";
-import {
-  getUsername,
-  globalReducer,
-  setUsername,
-  setValidFields,
-  validateFields,
-} from "./globalReducer";
-
-import { globalSelector } from "./globalSelector";
-
-export default function* appointmentSaga() {}
+export default function* globalSaga() {}
--- src/store/globalSelector.ts
+++ src/store/globalSelector.ts
@@ -4,24 +4,9 @@ export const globalSelector = (state: any) => state.global;
 
 export const mainSelector = (state: any) => state;
 
-export const userIdSelector = createSelector(
-  globalSelector,
-  (global) => global.userId
-);
-
 export const roleSelector = createSelector(
   globalSelector,
   (global) => global.role
-);
-
-export const usernameSelector = createSelector(
-  globalSelector,
-  (global) => global.username
-);
-
-export const validFieldSelector = createSelector(
-  globalSelector,
-  (global) => global.validFields
 );
 
 export const allLoadingSelector = createSelector(mainSelector, (state) => {
